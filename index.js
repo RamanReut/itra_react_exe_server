57b230ead1b52f034e78a592d562f3d270ed16fd
Raman Reut <r.reut@itransition.com>
2020-08-24 15:03:11 +0300

    add data for fake api

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 4000;
 
 app.use(express.static('public'));
+app.get('/api/orders', (req, resp) => {
+    resp.sendFile(__dirname+'/resp.json');
+});
 app.get('*', (req, resp) => {
-    resp.sendFile('public/index.html');
+    resp.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port);
